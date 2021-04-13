d077c9d9abc676b8d0efca47f1bf812125453bb5
harisfi <acc.cp.haris@gmail.com>
2021-04-13 14:37:26 +0700

    Update book model

--- app/src/main/java/com/hryzx/sourcbooks/models/Book.kt
+++ app/src/main/java/com/hryzx/sourcbooks/models/Book.kt
@@ -1,9 +1,23 @@
 package com.hryzx.sourcbooks.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Book (
-    var title: String = "",
-    var writer: String = "",
+    var key: String? = null,
+    var title: String? = null,
+    var writer: String? = null,
     var numPages: Int = 0,
-    var cover: Int = 0,
-    var bookmarked: Boolean = false
-)
+    var cover: Int? = -1,
+    var published: Int = 0,
+    var revision: Int = 0,
+    var publishers: String? = null,
+    var publishPlaces: String? = null,
+    var series: String? = null,
+    var contributions: String? = null,
+    var pagination: String? = null,
+    var languages: String? = null,
+    var bookmarked: Boolean = false,
+    var saved: Boolean = false
+) : Parcelable
