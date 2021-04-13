a0660f636055987eab45f3ab980ad41d4ca39262
harisfi <acc.cp.haris@gmail.com>
2021-04-13 14:41:41 +0700

    Update recycler view adapters

--- app/src/main/java/com/hryzx/sourcbooks/adapter/RecyclerViewGridAdapter.kt
+++ app/src/main/java/com/hryzx/sourcbooks/adapter/RecyclerViewGridAdapter.kt
@@ -6,28 +6,43 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.hryzx.sourcbooks.R
+import com.hryzx.sourcbooks.databinding.ItemBookGridBinding
 import com.hryzx.sourcbooks.models.Book
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 class RecyclerViewGridAdapter(private val listBook: ArrayList<Book>) : RecyclerView.Adapter<RecyclerViewGridAdapter.GridViewHolder>() {
+    private var onItemClickCallback: OnItemClickCallback? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_book_grid, parent, false)
-        return GridViewHolder(view)
+        val binding = ItemBookGridBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GridViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
-        val book = listBook[position]
-
-        Glide.with(holder.itemView.context)
-                .load(book.cover)
-                .into(holder.imgCover)
+        holder.bind(listBook[position])
     }
 
-    override fun getItemCount(): Int {
-        return listBook.size
+    override fun getItemCount(): Int = listBook.size
+
+    inner class GridViewHolder(private val binding: ItemBookGridBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(book: Book) {
+            Glide.with(itemView.context)
+                    .load(book.cover)
+                    .apply(RequestOptions.placeholderOf(R.drawable.ic_placehold))
+                    .error(R.drawable.ic_error)
+                    .into(binding.imgCoverGrid)
+            itemView.setOnClickListener { onItemClickCallback?.onItemClicked(book) }
+        }
     }
 
-    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgCover: ImageView = itemView.findViewById(R.id.img_cover_grid)
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Book)
     }
 }
--- app/src/main/java/com/hryzx/sourcbooks/adapter/RecyclerViewListAdapter.kt
+++ app/src/main/java/com/hryzx/sourcbooks/adapter/RecyclerViewListAdapter.kt
@@ -6,59 +6,55 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.hryzx.sourcbooks.R
+import com.hryzx.sourcbooks.databinding.ItemBookListBinding
 import com.hryzx.sourcbooks.models.Book
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
-class RecyclerViewListAdapter(private val listBook: ArrayList<Book>) : RecyclerView.Adapter<RecyclerViewListAdapter.ListViewHolder>() {
-    private lateinit var onItemClickCallback: OnItemClickCallback
+class RecyclerViewListAdapter() : RecyclerView.Adapter<RecyclerViewListAdapter.ListViewHolder>() {
+    private var onItemClickCallback: OnItemClickCallback? = null
+    private val listBook = ArrayList<Book>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewListAdapter.ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_book_list, parent, false)
-        return ListViewHolder(view)
+    fun setData(items: ArrayList<Book>) {
+        listBook.clear()
+        listBook.addAll(items)
+        notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: RecyclerViewListAdapter.ListViewHolder, position: Int) {
-        val book = listBook[position]
-
-        Glide.with(holder.itemView.context)
-                .load(book.cover)
-                .into(holder.imgCover)
-
-        holder.txTitle.text = book.title
-        holder.txWriter.text = "by ${book.writer}"
-//        val numPage = "${book.numPages} pages"
-        holder.txNumPage.text = "${book.numPages} pages"
-        holder.ratingBook.rating = 2.5F
-
-        holder.imgBookmark.setOnClickListener {
-            onItemClickCallback.onItemClicked(listBook.get(holder.adapterPosition))
-        }
-
-        holder.txTitle.setOnClickListener {
-            Toast.makeText(
-                holder.itemView.context,
-                book.title,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        holder.imgBookmark.setOnClickListener {
-            holder.imgBookmark.setImageResource(if (book.bookmarked) R.drawable.ic_bookmark_border_24 else R.drawable.ic_bookmark_24)
-            book.bookmarked = !book.bookmarked
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemBookListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return listBook.size
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listBook[position])
     }
 
-    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgCover: ImageView = itemView.findViewById(R.id.img_cover)
-        var txTitle: TextView = itemView.findViewById(R.id.txTitle)
-        var txWriter: TextView = itemView.findViewById(R.id.txWriter)
-        var txNumPage: TextView = itemView.findViewById(R.id.txNumPage)
-        var imgBookmark: ImageButton = itemView.findViewById(R.id.img_bookmark)
-        var ratingBook: RatingBar = itemView.findViewById(R.id.rating)
+    override fun getItemCount(): Int = listBook.size
+
+    inner class ListViewHolder(private val binding: ItemBookListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(book: Book) {
+            with(binding) {
+                Glide.with(itemView.context)
+                        .load("http://covers.openlibrary.org/b/id/${book.cover}-M.jpg")
+                        .apply(RequestOptions.placeholderOf(R.drawable.ic_placehold))
+                        .error(R.drawable.ic_error)
+                        .into(imgCover)
+
+                txTitle.text = book.title
+                txWriter.text = root.context.getString(R.string.by_, book.writer)
+                txNumPage.text = root.context.getString(R.string.page_, book.numPages)
+
+                imgBookmark.setOnClickListener {
+                    imgBookmark.setImageResource(if (book.bookmarked) R.drawable.ic_bookmark_border_24 else R.drawable.ic_bookmark_24)
+                    book.bookmarked = !book.bookmarked
+                }
+
+                itemView.setOnClickListener { onItemClickCallback?.onItemClicked(book) }
+            }
+        }
     }
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
