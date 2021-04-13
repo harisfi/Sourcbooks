ef349a1fe7e6aeffe4863fd36c97d80dc80a5bee
harisfi <acc.cp.haris@gmail.com>
2021-04-13 14:42:13 +0700

    Update home fragment

--- app/src/main/java/com/hryzx/sourcbooks/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hryzx/sourcbooks/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.hryzx.sourcbooks.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +14,20 @@ import com.hryzx.sourcbooks.adapter.RecyclerViewListAdapter
 import com.hryzx.sourcbooks.adapter.TabPagerAdapter
 import com.hryzx.sourcbooks.databinding.FragmentHomeBinding
 import com.hryzx.sourcbooks.models.Book
+import com.hryzx.sourcbooks.ui.BookDetailActivity
 import com.hryzx.sourcbooks.ui.home.item1.HomeItem1Fragment
 import com.hryzx.sourcbooks.ui.home.item2.HomeItem2Fragment
 import com.hryzx.sourcbooks.ui.home.item3.HomeItem3Fragment
 import com.hryzx.sourcbooks.ui.home.item4.HomeItem4Fragment
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
 //    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var rvAdapter: RecyclerViewListAdapter
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -41,19 +50,20 @@ class HomeFragment : Fragment() {
         binding.viewpagerMain.adapter = adapter
         binding.tabsMain.setupWithViewPager(binding.viewpagerMain)
 
-        val list = arrayListOf<Book>()
-        for (i in 1..5) {
-            val book = Book()
-            book.title = "A falsis, itineris tramitem placidus onus.$i"
-            book.cover = R.drawable.sample_book
-            book.writer = "Pol.$i"
-            book.numPages = 981 + i
-            list.add(book)
-        }
+        rvAdapter = RecyclerViewListAdapter()
+        rvAdapter.notifyDataSetChanged()
 
         binding.rvHomeBooks.layoutManager = LinearLayoutManager(view.context)
-        val mainRecyclerViewAdapter = RecyclerViewListAdapter(list)
-        binding.rvHomeBooks.adapter = mainRecyclerViewAdapter
+        binding.rvHomeBooks.adapter = rvAdapter
+        setRecommendation()
+
+        rvAdapter.setOnItemClickCallback(object : RecyclerViewListAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Book) {
+                val intent = Intent(activity, BookDetailActivity::class.java)
+                intent.putExtra(BookDetailActivity.EXTRA_BOOK, data)
+                startActivity(intent)
+            }
+        })
 
 //        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -66,6 +76,47 @@ class HomeFragment : Fragment() {
         })*/
     }
 
+    private fun setRecommendation() {
+        val listItems = ArrayList<Book>()
+        val url = "https://openlibrary.org/subjects/painting__paintings.json?limit=7"
+
+        val client = AsyncHttpClient()
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray) {
+                try {
+                    val result = String(responseBody)
+                    val responseObject = JSONObject(result)
+                    val list = responseObject.getJSONArray("works")
+
+                    for (i in 0 until list.length()) {
+                        val item = list.getJSONObject(i)
+                        val bookItem = Book()
+                        val author = item.getJSONArray("authors")
+                                .getJSONObject(0)
+                                .getString("name")
+
+                        with(bookItem) {
+                            key = item.getString("key")
+                            title = item.getString("title")
+                            if (!item.isNull("cover_id")) {
+                                cover = item.getInt("cover_id")
+                            }
+                            writer = author
+                        }
+                        listItems.add(bookItem)
+                    }
+                    rvAdapter.setData(listItems)
+                } catch (e: Exception) {
+                    Log.d("Exception", e.message.toString())
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable) {
+                Log.d("onFailure", error.message.toString())
+            }
+
+        })
+    }
 
     override fun onDestroy() {
         super.onDestroy()
